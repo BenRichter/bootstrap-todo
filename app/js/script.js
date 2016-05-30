19eaee0ec48022d19daccf640c218ba3001f9338
Ben <ben.k.richter@gmail.com>
2016-05-30 22:49:47 +0200

    local storage

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,43 @@
 /**
  * Created by Ben on 29.05.2016.
  */
-console.log("BEnson");
+// immediately invoked, $ locally scoped
+(function ($, window, document) {
+    "use strict";  // for dev: throw more errors > fail fast, fail loudly
+
+    // DOM ready
+    $(document).ready(function () {
+        // Initial loading of tasks
+        var i;
+
+        for (i = 0; i < localStorage.length; i++){
+            $("#tasks").append("<li id='task-" + i + "'>" + localStorage.getItem('task-' + i) + " <a href='#'>Delete</a></li>");
+        }
+
+        // Add a task
+        $("#tasks-form").submit(function () {
+            if ($("#task").val() !== "") {
+                localStorage.setItem("task-" + i, $("#task").val());
+
+                $("#tasks").append("<li id='task-" + i + "'>" + localStorage.getItem("task-" + i) +" <a href='#'>Delete</a></li>");
+                $("#task").val("");
+                i++;
+            }
+            return false;
+        });
+
+
+        // Remove a task
+        $("#tasks li a").on("click", function () {
+            localStorage.removeItem($(this).parent().attr("id"));
+            $(this).parent().hide();
+            for (i = 0; i < localStorage.length; i++) {
+                if (!localStorage.getItem("task-" + i)) {
+                    localStorage.setItem("task-" + i, localStorage.getItem('task-' + (i + 1)));
+                    localStorage.removeItem('task-' + (i + 1));
+                }
+            }
+        });
+    });
+
+}(window.jQuery, window, document));
--- gulpfile.js
+++ gulpfile.js
@@ -8,69 +8,68 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),  //
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     sourcemaps = require('gulp-sourcemaps'),
+    sourcemaps = require('gulp-sourcemaps'),
     less = require('gulp-less'),
     del = require('del');
-
-
 
 gulp.task('styles', function () {
     gulp.src('app/less/**/*.less')
         .pipe(sourcemaps.init())
         .pipe(less())
-
         .pipe(sourcemaps.write())
         .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(gulp.dest('build/assets/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(gulp.dest('build/assets/css'))
+        .pipe(livereload());
 });
 
 
 gulp.task('scripts', function() {
-    return gulp.src('app/scripts/**/*.js')
+    return gulp.src('app/js/**/*.js')
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(gulp.dest('build/assets/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/js'))
-        .pipe(notify({ message: 'Scripts task complete' }));
+        .pipe(gulp.dest('build/assets/js'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/*html')
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('images', function() {
     return gulp.src('src/images/**/*')
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('dist/assets/img'))
+        .pipe(gulp.dest('build/assets/img'))
         .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('clean', function() {
-    return del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img']);
+    return del(['build']);
 });
 
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts'); // ,'images'  no images yet
+    gulp.start('styles', 'scripts', 'html'); // ,'images'  no images yet
 });
-
-
 
 // Watch
 gulp.task('watch', function() {
-
-    // Watch .less files
-    gulp.watch('app/styles/**/*.less', ['styles']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
-    //gulp.watch('app/images/**/*', ['images']);
-
     // Create LiveReload server
     livereload.listen();
 
-    // Watch any files in dist/, reload on change
-    gulp.watch(['dist/**']).on('change', livereload.changed);
+    // Watch .less files
+    gulp.watch('app/less/**/*.less', ['styles']);
+    gulp.watch('app/js/**/*.js', ['scripts']);
+    //gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('app/*.html', ['html']);
+
+    // Watch any files in build/, reload on change
+    gulp.watch(['build/**']).on('change', livereload.changed);
 });
